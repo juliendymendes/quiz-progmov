5a628a1590604b3b1fa5a3c6d67fd3fc8dd27ff4
Juliendy Mendes <juliendy0708@gmail.com>
2022-06-18 14:02:38 -0400

    classe de banco de dados, entidades e helpers

---
+++ app/src/main/java/com/me/quiz/entidades/Quiz.java
@@ -0,0 +1,19 @@
+package com.me.quiz.entidades;
+
+public class Quiz {
+    private int idQuiz;
+
+    public Quiz(){}
+
+    public Quiz(int idQuiz) {
+        this.idQuiz = idQuiz;
+    }
+
+    public int getIdQuiz() {
+        return idQuiz;
+    }
+
+    public void setIdQuiz(int idQuiz) {
+        this.idQuiz = idQuiz;
+    }
+}
