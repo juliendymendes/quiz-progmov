748632ba66c7a5bcd2c099fbffcf7640b8bdd2eb
Juliendy Mendes <juliendy0708@gmail.com>
2022-07-02 21:47:35 -0400

    carregando questoes na tela

--- app/src/main/java/com/me/quiz/telas/QuizFragment.java
+++ app/src/main/java/com/me/quiz/telas/QuizFragment.java
@@ -1,21 +1,37 @@
 package com.me.quiz.telas;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
-
+import com.me.quiz.R;
 import com.me.quiz.databinding.FragmentQuizBinding;
 import com.me.quiz.entidades.Questao;
+import com.me.quiz.entidades.Quiz;
+import com.me.quiz.entidades.Usuario;
+import com.me.quiz.entidades.UsuarioLogado;
+import com.me.quiz.helpers.QuizHelper;
+import com.me.quiz.helpers.QuizUsuarioHelper;
+import com.me.quiz.helpers.UsuarioHelper;
 import com.me.quiz.servicos.RetrofitClient;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -24,7 +40,27 @@ import retrofit2.Response;
 
 public class QuizFragment extends Fragment {
     FragmentQuizBinding binding;
+    int indexQuestaoAtual = 0;
+    int acertos;
+    Questao questaoAtual;
+    Date date;
+    String categoria;
 
+    Long tempoInicio;
+    Long tempoFim;
+    Long tempoFinal;
+
+    QuizUsuarioHelper quizUsuarioHelper;
+    QuizHelper quizHelper;
+    UsuarioHelper usuarioHelper;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        quizUsuarioHelper = new QuizUsuarioHelper(getContext());
+        quizHelper = new QuizHelper(getContext());
+        usuarioHelper = new UsuarioHelper(getContext());
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,32 +69,106 @@ public class QuizFragment extends Fragment {
         return binding.getRoot();
     }
 
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        date = new Date();
+        tempoInicio = System.currentTimeMillis();
 
-        binding.rgAlternativas.setOnCheckedChangeListener((radioGroup, i) -> {
-            System.out.println(i);
-        });
+        Bundle bundle = getArguments();
+        assert bundle != null;
+        categoria = bundle.getString("categoria");
 
-        getQuestoes();
+        getQuestoesPorCategorias(categoria);
+
     }
 
-    private void getQuestoes() {
-        Call<List<Questao>> call = RetrofitClient.getInstance().getApi().getQuestoes();
-        call.enqueue(new Callback<List<Questao>>() {
-            @Override
-            public void onResponse(Call<List<Questao>> call, Response<List<Questao>> response) {
-                List<Questao> questoes = response.body();
 
-               binding.tvDescricao.setText(questoes.get(0).getQuestao());
+    private void getQuestoesPorCategorias(String categoria) {
+       Call<ArrayList<Questao>> call = RetrofitClient.getInstance().getApi().getQuestoesPorCategoria(categoria,10);
+        call.enqueue(new Callback<ArrayList<Questao>>() {
+            @Override
+            public void onResponse(@NonNull Call<ArrayList<Questao>> call, @NonNull Response<ArrayList<Questao>> response) {
+                ArrayList<Questao> questoes = response.body();
+                assert questoes != null;
+
+                questaoAtual = questoes.get(indexQuestaoAtual);
+                carregaQuestao(questaoAtual);
+                indexQuestaoAtual += 1;
+                binding.btnProxima.setOnClickListener(view -> {
+                    verificaResposta();
+                   if(indexQuestaoAtual < 10){
+                       if(binding.rgAlternativas.getCheckedRadioButtonId() != -1) {
+
+                           questaoAtual = questoes.get(indexQuestaoAtual);
+                           indexQuestaoAtual += 1;
+                           carregaQuestao(questaoAtual);
+
+                       }else {
+                           Toast.makeText(getContext(), "Selecione uma resposta!", Toast.LENGTH_SHORT).show();
+                       }
+                   }else{
+
+
+                        navegaParaTelaResultados();
+                   }
+                });
             }
 
             @Override
-            public void onFailure(Call<List<Questao>> call, Throwable t) {
+            public void onFailure(@NonNull Call<ArrayList<Questao>> call, Throwable t) {
                 Toast.makeText(getContext(), "Ops, algo deu errado. Tente novamente.", Toast.LENGTH_LONG).show();
             }
 
+
+            public void carregaQuestao(Questao questaoAtual){
+                    binding.rgAlternativas.clearCheck();
+                    
+                    binding.tvDescricao.setText(questaoAtual.getQuestao());
+                    binding.op1.setText(questaoAtual.getRespostasIncorretas().get(0));
+                    binding.op2.setText(questaoAtual.getRespostasIncorretas().get(1));
+                    binding.op3.setText(questaoAtual.getRespostasIncorretas().get(2));
+                    binding.op4.setText(questaoAtual.getRespostaCorreta());
+            }
+
+            public void verificaResposta(){
+                RadioButton rb = getActivity().findViewById(binding.rgAlternativas.getCheckedRadioButtonId());
+
+                String resposta = rb.getText().toString();
+                if(resposta.equals(questaoAtual.getRespostaCorreta())){
+                    acertos +=1;
+                    System.out.println("Acertou ein");
+                }
+            }
+
+            public void navegaParaTelaResultados(){
+                tempoFim = date.getTime();
+                tempoFinal = (System.currentTimeMillis() - tempoInicio)/1000;
+
+                Quiz quiz = quizHelper.getQuizPorNome(categoria);
+                quiz.setAcertos(acertos);
+                quiz.setTempo(tempoFinal);
+                quizHelper.atualizarQuiz(quiz);
+                quizUsuarioHelper.inserirQuizParaUsuario(quiz, UsuarioLogado.getInstancia());
+                UsuarioLogado.getInstancia().setQtsAcertos(acertos);
+                usuarioHelper.atualizarUsuario(UsuarioLogado.getInstancia());
+
+                assert getParentFragment() != null;
+                Bundle bundle = new Bundle();
+                bundle.putInt("acertos", acertos);
+                bundle.putLong("tempo", tempoFinal);
+                if(acertos >= 6){
+                    NavHostFragment.findNavController(getParentFragment()).navigate(R.id.action_quizFragment_to_resultadoPositivoFragment, bundle);
+                }else {
+                    NavHostFragment.findNavController(getParentFragment()).navigate(R.id.action_quizFragment_to_resultadoNegativoFragment, bundle);
+                }
+                System.out.println(acertos);
+            }
+
+
+
         });
+
     }
 }
