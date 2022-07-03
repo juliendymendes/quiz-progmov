4bbaba1b24090360938c009117260ce48dda0e0b
Juliendy Mendes <juliendy0708@gmail.com>
2022-07-02 22:05:10 -0400

    navegação da tela de perfil para activity tirar foto

---
+++ app/src/main/java/com/me/quiz/TirarFoto.java
@@ -0,0 +1,14 @@
+package com.me.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TirarFoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tirar_foto);
+    }
+}
